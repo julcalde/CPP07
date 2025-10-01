16b77cc73dc38fe747983e0bf2ae431c1a7f5dc9
julcalde <julcalde@student.42heilbronn.de>
2025-10-01 15:53:47 +0200

    ex00: executable renamed. Main function showcases templates use on integers and strings

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,14 +6,36 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 15:33:41 by julcalde          #+#    #+#             */
-/*   Updated: 2025/10/01 15:33:59 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/10/01 15:51:40 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+#include <iostream>
+#include <string>
 
 int main(void)
 {
-
+	std::cout << "\\---Test with integers---\\\n" << std::endl;
+	int a = 2, b = 3;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+	
+	std::cout << "\n\\---Test with strings---\\\n" << std::endl;
+	std::string s1 = "dog";
+	std::string s2 = "dawg";
+	std::cout << "s1 = " << s1 << ", s2 = " << s2 << std::endl;
+	std::cout << "min(s1, s2) = " << ::min(s1, s2) << std::endl;
+	std::cout << "max(s1, s2) = " << ::max(s1, s2) << std::endl;
+	::swap(s1, s2);
+	std::cout << "s1 = " << s1 << ", s2 = " << s2 << std::endl;
+	std::cout << "min(s1, s2) = " << ::min(s1, s2) << std::endl;
+	std::cout << "max(s1, s2) = " << ::max(s1, s2) << std::endl;
+	
 	return (0);
 }
