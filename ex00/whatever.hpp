dd6039249ae786f3ee4044281b484f71351d8993
julcalde <julcalde@student.42heilbronn.de>
2025-10-01 15:37:57 +0200

    ex00: template created

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,13 +6,31 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 15:34:02 by julcalde          #+#    #+#             */
-/*   Updated: 2025/10/01 15:34:23 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/10/01 15:37:21 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
+template <typename T>
+void swap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
 
+template <typename T>
+T min(const T& a, const T& b)
+{
+	return ((a < b) ? a : b);
+}
+
+template <typename T>
+T max(const T& a, const T& b)
+{
+	return ((a > b) ? a : b);
+}
 
 #endif
