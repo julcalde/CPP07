dc06d9d14dacfe8008549ad9ebd241d51d5dea69
julcalde <julcalde@student.42heilbronn.de>
2025-10-05 17:16:43 +0200

    ex01: [iter.hpp] created templates for const and non-const arrays with validity check and for loop to go through the arrays. [main.cpp] wrote some tests for testing a printing function that prints all the elements of an array and a function to increment each element of an array. The test shows also the behavior in case of invalid inputs.

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,13 +6,29 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 16:53:54 by julcalde          #+#    #+#             */
-/*   Updated: 2025/10/05 16:54:05 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/10/05 16:59:33 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
+template <typename T>
+void iter(T* array, const int length, void (*function)(T&))
+{
+	if (!array || length <= 0)
+		return ;
+	for (int i = 0; i < length; ++i)
+		function(array[i]);
+}
 
+template <typename T>
+void iter(const T* array, const int length, void (*function)(const T&))
+{
+	if (!array || length <= 0)
+		return ;
+	for (int i = 0; i < length; ++i)
+		function(array[i]);
+}
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,14 +6,48 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 16:53:51 by julcalde          #+#    #+#             */
-/*   Updated: 2025/10/05 16:54:19 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/10/05 17:12:33 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
+#include <iostream>
+#include <string>
+
+template <typename T>
+void printMisc(const T& misc)
+{
+	std::cout << misc << std::endl;
+}
+
+template <typename T>
+void incrementMisc(T& misc)
+{
+	misc++;
+}
 
 int main(void)
 {
+	std::string strArray[] = {"Hello", "I'm", "under", "water.", "Please", "help", "me"};
+	std::cout << "\\---Test with string array---\\\n" << std::endl;
+	iter(strArray, 7, printMisc);
+
+	int intArray[]= {0, 9, 21, 42, 1337};
+	std::cout << "\n\\---Test with int array---\\\n" << std::endl;
+	std::cout << "original array:" << std::endl;
+	iter(intArray, 5, printMisc);
+	std::cout << "\narray after incrementing each element:" << std::endl;
+	iter(intArray, 5, incrementMisc);
+	iter(intArray, 5, printMisc);
+
+	const int constIntArray[]= {0, 9, 21, 42, 1337};
+	std::cout << "\n\\---Test with const int array---\\\n" << std::endl;
+	iter(constIntArray, 5, printMisc);
+
+// note: invalid inputs test should print nothing
+	std::cout << "\n\\---Test invalid inputs---\\\n" << std::endl;
+	iter((int*)0, 5, printMisc);
+	iter(intArray, -1, printMisc);
 	
 	return (0);
 }
