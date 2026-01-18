cd8210445ff7f5efb932ccfa16e391998e84b647
Julio Max Calderone Da Silva <julcalde@3-D-8.42heilbronn.de>
2026-01-19 00:28:25 +0100

    ex00: ignore

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 15:34:02 by julcalde          #+#    #+#             */
-/*   Updated: 2025/10/01 15:39:28 by julcalde         ###   ########.fr       */
+/*   Updated: 2026/01/19 00:25:50 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 /* Templates
 	Templates are a powerful feature in C++ that allows you to write generic and reusable code.
-	They enable you to create functions and classes that can operate with any data type without 
-	being tied to a specific one. This is particularly useful when you want to implement 
-	algorithms or data structures that work with different types of data.
+	They enable the creation of functions and classes that can operate with any data type without 
+	being tied to a specific one. This is particularly useful when an implementation needs to be 
+	generic and work with different types.
 
-	In this example, we define three template functions: swap, min, and max.
+	In this example, I defined three template functions: swap(), min(), and max().
 	1. swap: This function takes two references of type T and swaps their values.
 	2. min: This function takes two constant references of type T and returns the smaller of the two values.
 	3. max: This function takes two constant references of type T and returns the larger of the two values.
