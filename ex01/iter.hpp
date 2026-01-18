476cd76e6d86f50386ff1a10cb1f2c7f6a241768
Julio Max Calderone Da Silva <julcalde@3-D-8.42heilbronn.de>
2026-01-19 00:27:34 +0100

    ex01: Added comments for clarity

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,13 +6,16 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 16:53:54 by julcalde          #+#    #+#             */
-/*   Updated: 2025/10/05 16:59:33 by julcalde         ###   ########.fr       */
+/*   Updated: 2026/01/19 00:24:34 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
+// While iterating over an array, the iter function applies the function
+// passed as a parameter to each element of the array.
+// This overload allows modification of the elements.
 template <typename T>
 void iter(T* array, const int length, void (*function)(T&))
 {
@@ -22,6 +25,9 @@ void iter(T* array, const int length, void (*function)(T&))
 		function(array[i]);
 }
 
+// While iterating over a constant array, the iter function applies the function
+// passed as a parameter to each element of the array.
+// This overload ensures that the function does not modify the elements.
 template <typename T>
 void iter(const T* array, const int length, void (*function)(const T&))
 {
